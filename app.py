f3361a1a5bc0a93e9cbe050934d2ee8c8eb1da8e
Sahil Sharma <sahilsharmamrp@gmail.com>
2026-01-04 13:34:38 +0530

    Deployment fix

--- app.py
+++ app.py
@@ -833,4 +833,6 @@ def health_check():
         }), 503
 
 if __name__ == '__main__':
-    app.run(debug=config.DEBUG, host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
